1e00190527782465e73b1e1e2f5a56ba44a4ca0f
Zephyro Kemstedt <zkemstedt@gmail.com>
2020-10-06 09:01:27 +0200

    lesson1

--- lesson_1/main.py
+++ lesson_1/main.py
@@ -4,7 +4,21 @@
 
 import tkinter as tk
 
-
 window = tk.Tk()
-greeting = tk.Label(text='Hello, Tkinter')
-greeting.pack()
+
+
+def send_text() -> None:
+    text_field.insert(tk.END, f'{text_entry.get()}\n')
+
+
+title = tk.Label(text='This is the program name')
+text_entry = tk.Entry(width=50)
+send_button = tk.Button(text='send', height=7, width=25, command=send_text)
+text_field = tk.Text()
+
+title.pack()
+text_entry.pack()
+send_button.pack()
+text_field.pack()
+
+window.mainloop()
