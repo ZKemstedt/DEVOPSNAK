9218e16949d4addc74ff5e7f0efe1cdb2ecdaca3
Zephyro Kemstedt <zkemstedt@gmail.com>
2020-10-06 13:47:22 +0200

    add raise TypeError if any arg is not int. for the math functions in utils.

--- lesson_2/utils.py
+++ lesson_2/utils.py
@@ -2,15 +2,21 @@ from functools import reduce
 
 
 def multiply(*factor):
+    if not all(isinstance(f, int) for f in factor):
+        raise TypeError('arguments must be int')
     return reduce(lambda x, y: x*y, factor)
 
 
 def add(*term):
+    if not all(isinstance(t, int) for t in term):
+        raise TypeError('arguments must be int')
     return sum(term)
 
 
-def sum_inc_five(*factor):
-    return add((*factor), 5)
+def sum_inc_five(*term):
+    if not all(isinstance(t, int) for t in term):
+        raise TypeError('arguments must be int')
+    return add((*term), 5)
 
 
 def decide_branch(decide):
