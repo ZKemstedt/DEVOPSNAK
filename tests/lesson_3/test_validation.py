850bb098ccd440ef1f717305864b5e6c7a79b8b8
Zephyro Kemstedt <zkemstedt@gmail.com>
2020-10-13 09:29:15 +0200

    update to match update to validate_conf

--- tests/lesson_3/test_validation.py
+++ tests/lesson_3/test_validation.py
@@ -32,8 +32,8 @@ class ValidationTests(unittest.TestCase):
         with self.assertRaises(TypeError):
             validation.validate_conf(5)
 
-    def test_validate_conf(self):
-        self.assertEqual(validation.validate_conf('string'), None)
+    def test_validate_conf_is_string(self):
+        self.assertEqual(validation.validate_conf('key', 'value'), None)
 
 # *   validation.validate_args
 #     *   Den kan hantera de options som finns.
