b643c5fedc5fb81d8a7fbe106445ca3c042e653c
Robert Westin <70424136+robert-alfwar@users.noreply.github.com>
2020-10-16 11:10:33 +0200

    more material

--- lesson_6/pizza.py
+++ lesson_6/pizza.py
@@ -1,7 +1,28 @@
 class Pizza:
+    price_groups = {
+        1: 80,
+        2: 90
+    }
+
+    ingredients_valuation = {
+        "mozzarella": 1,
+        "pepperoni": 1
+    }
 
     def __init__(self):
         self.ingredients = ["cheese", "tomato"]
 
     def add_ingredients(self, ingredients):
         self.ingredients.extend(ingredients)
+
+    @property
+    def price_group(self):
+        price_group = 1
+        for ingredient in self.ingredients:
+            if(ingredient in Pizza.ingredients_valuation):
+                price_group += Pizza.ingredients_valuation[ingredient]
+        return price_group
+
+    @property
+    def price(self):
+        return Pizza.price_groups[self.price_group]
--- tests/lesson_6/test_pizza.py
+++ tests/lesson_6/test_pizza.py
@@ -7,16 +7,52 @@ class TestPizza(unittest.TestCase):
     def setUp(self):
         self.pizza = Pizza()
 
-    def test_pizza_create(self):
+    def test_pizza_create_success(self):
         self.assertIsInstance(self.pizza, Pizza)
 
-    def test_pizza_create_with_ingredients(self):
+    def test_pizza_create_with_ingredients_return_list(self):
         ingredients = ["mushroom", "ham"]
         self.pizza.add_ingredients(ingredients)
         self.assertListEqual(self.pizza.ingredients, ["cheese", "tomato", *ingredients])
 
-    def test_pizza_create_default_ingredients(self):
+    def test_pizza_create_default_ingredients_return_list(self):
         self.assertListEqual(self.pizza.ingredients, ["cheese", "tomato"])
+
+    # UnitOfWork_StateUnderTest_ExpectedBehavior
+    def test_pizza_price_group_default_1(self):
+        self.assertEqual(self.pizza.price_group, 1)
+
+    def test_pizza_price_price_group_1_returns_price(self):
+        self.assertEqual(self.pizza.price, 80)
+
+    def test_Pizza_price_groups_exists(self):
+        self.assertIsInstance(Pizza.price_groups, dict)
+
+    def test_Pizza_price_groups_1_is_int(self):
+        self.assertIsInstance(Pizza.price_groups[1], int)
+
+    def test_pizza_add_pepperoni_returns_price_group_2(self):
+        ingredients = ["pepperoni"]
+        self.pizza.add_ingredients(ingredients)
+        self.assertEqual(self.pizza.price_group, 2)
+
+    def test_pizza_price_price_group_2_returns_price(self):
+        ingredients = ["pepperoni"]
+        self.pizza.add_ingredients(ingredients)
+        self.assertEqual(self.pizza.price, 90)
+
+    def test_pizza_ingredients_valuation_dict(self):
+        self.assertIsInstance(Pizza.ingredients_valuation, dict)
+
+    def test_pizza_mozzarella_returns_price_group_2(self):
+        ingredients = ["mozzarella"]
+        self.pizza.add_ingredients(ingredients)
+        self.assertEqual(self.pizza.price_group, 2)
+
+    def test_pizza_mozzarella_and_pepperoni_returns_price_group_3(self):
+        ingredients = ["mozzarella", "pepperoni"]
+        self.pizza.add_ingredients(ingredients)
+        self.assertEqual(self.pizza.price_group, 3)
 
 
 if __name__ == "__main__":
