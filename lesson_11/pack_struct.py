37d65aabc3b4499b67c9ee8275165e81e5d6e3e4
Robert Westin <70424136+robert-alfwar@users.noreply.github.com>
2020-10-27 14:39:19 +0100

    pickle example & struct

--- lesson_11/pack_struct.py
+++ lesson_11/pack_struct.py
@@ -1,27 +1,36 @@
 import struct
+import pickle
 
 # Setup
 TEXT = b"t"
-print(struct.calcsize('!Lc'))
+DICT = b"d"
+print(1, struct.calcsize('!Lc'))
 
-message_type = TEXT
-message = "hello world"
-message_as_bytes = message.encode()
+message_type = DICT
+message = {"key": "hello world åäö"}
+message_as_bytes = pickle.dumps(message)
 
 # Pack message
 struct_format = f'!Lc{len(message_as_bytes)}s'
 struct_size = struct.calcsize(f'!Lc{len(message_as_bytes)}s')
-print("struct_format", struct_format)
-print("struct_size", struct_size)
+print(2, "struct_format", struct_format)
+print(3, "struct_size", struct_size)
 
 packed_message = struct.pack(struct_format, struct_size, message_type, message_as_bytes)
-print(packed_message)
-print(f"packed_message len {len(packed_message)}")
+print(4, packed_message)
+print(5, f"packed_message len {len(packed_message)}")
+
+# Send over socket packed_message
+
+# Recv from socket
 
 # Unpack
 message_length, message_type = struct.unpack('!Lc', packed_message[0:5])
-print(struct.calcsize('!Lc'))
+print(6, struct.calcsize('!Lc'))
 message,  = struct.unpack(f"!{message_length-5}s", packed_message[5:message_length])
 
-if(message_type == b"t"):
-    print(message.decode())
+if(message_type == TEXT):
+    print(7, message.decode())
+
+if(message_type == DICT):
+    print(8, pickle.loads(message))
--- lesson_11/to_bytes.py
+++ lesson_11/to_bytes.py
@@ -3,17 +3,19 @@ import json
 
 
 a_list = ["hello", "world", 1, "åäö", 3]
-print(type(a_list))
+print(1, type(a_list))
 
 serialized_with_pickle = pickle.dumps(a_list)
-print(type(serialized_with_pickle))
-print(serialized_with_pickle)
+print(2, type(serialized_with_pickle))
+print(3, serialized_with_pickle)
+print("3a", len(serialized_with_pickle))
 
 
 serialized_with_json = json.dumps({"a_list": a_list})
-print(type(serialized_with_json))
-print(serialized_with_json)
+print(4, type(serialized_with_json))
+print(5, serialized_with_json)
 
 json_to_bytes = serialized_with_json.encode()
-print(json_to_bytes)
-print(type(json_to_bytes))
+print(6, json_to_bytes)
+print(7, type(json_to_bytes))
+print(8, len(json_to_bytes))
