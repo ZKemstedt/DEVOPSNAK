2c3587ed7ef4fc13a40a37c17615dfc64e692073
Zephyro Kemstedt <zkemstedt@gmail.com>
2020-10-29 10:29:35 +0100

    move create_request & create_response to shared.utils

--- shared/utils.py
+++ shared/utils.py
@@ -13,3 +13,21 @@ def json_decode(json_bytes, encoding):
     with io.TextIOWrapper(io.BytesIO(json_bytes), encoding=encoding, newline='') as tiow:
         obj = json.load(tiow)
     return obj
+
+
+def create_request(action, value):
+    content = {'action': action, 'value': value}
+    return {
+        'content-type': 'text/json',
+        'content-encoding': 'utf-8',
+        'content-bytes': json_encode(content, 'utf-8'),
+    }
+
+
+def create_response(result, data):
+    content = {'result': result, 'data': data}
+    return {
+        'content-type': 'text/json',
+        'content-encoding': 'utf-8',
+        'content-bytes': json_encode(content, 'utf-8'),
+    }
