39c04aa9dc15376b888ded58db3e40768128731c
Robert Westin <70424136+robert-alfwar@users.noreply.github.com>
2020-10-29 15:41:23 +0100

    magic mock example

--- tests/lesson_13/test_socket_to_test.py
+++ tests/lesson_13/test_socket_to_test.py
@@ -6,9 +6,9 @@ from lesson_13.socket_to_test import send_message
 class TestHappySock(unittest.TestCase):
 
     def test_send_msg_as_bytes(self):
-        my_mock = MagicMock()
-        send_message(my_mock, "hello world")
-        my_mock.send.assert_called_once_with(b"hello world")
+        my_mock_client = MagicMock()
+        send_message(my_mock_client, "hello world")
+        my_mock_client.send.assert_called_once_with(b"hello world")
 
 
 if __name__ == "__main__":
