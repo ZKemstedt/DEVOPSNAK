76054ba6a88ea641ba24b8a3c715b4d919573988
Zephyro <zkemstedt@gmail.com>
2021-08-27 14:53:18 +0200

    running/stopping snippets

--- Linux 2/snippets.sh
+++ Linux 2/snippets.sh
@@ -1,4 +1,5 @@
 # clean old images
+# https://www.digitalocean.com/community/tutorials/how-to-remove-docker-images-containers-and-volumes#:~:text=Remove%20a%20container%20upon%20exit,docker%20run%20%2D%2Drm%20image_name
 docker images | grep "<none>" | awk '{print $3}' | xargs docker rmi;
 
 # Copy public key
@@ -7,10 +8,18 @@ docker cp keys/dekstop.pub linux2_lab_1:/home/zephyro/dekstop.pub
 # ssh key
 ssh-keygen -t ed25519
 
+# Running, Stopping containers
+# https://docs.docker.com/compose/gettingstarted/
+docker-compose up
+    --build
+    -d, -detach
 
-# build new image and run new container
-docker-compose up --build
+docker-compose stop
+docker-compose down
+    --volumes
 
-# buildkit global env
+
+
+# to enable buildkit on windows
 # setx DOCKER_BUILDKIT 1
 # setx COMPOSE_DOCKER_CLI_BUILD 1
